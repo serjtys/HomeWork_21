2c8a59462f0f36d4683d9253426ed0d2c3ed6f26
serj <sergejtustin601@gmail.com>
2025-08-20 03:47:00 +0300

    create homework_24

--- catalog/urls.py
+++ catalog/urls.py
@@ -3,6 +3,8 @@ from . import views
 
 urlpatterns = [
     path('', views.home, name='home'),
-    path('catalog/', views.home, name='catalog'),  # Можно заменить на отдельный view
+    path('catalog/', views.home, name='catalog'),  # ← ЭТУ СТРОКУ НУЖНО ВЕРНУТЬ!
     path('contacts/', views.contacts, name='contacts'),
+    path('product/<int:pk>/', views.product_detail, name='product_detail'),
+    path('add-product/', views.add_product, name='add_product'),
 ]
